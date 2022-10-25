a27eb50198c44232dd3c483702b65bc6a64ae4eb
JuskingA <juskingadjeinyarko@gmail.com>
2022-10-25 21:12:40 +0100

    9 hours to go

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Module for number_of_lines method"""
-
-
 def number_of_lines(filename=""):
-    """Method that returns the number of lines of a text file"""
-    with open(filename, encoding="utf-8") as my_file_0:
-        lineNum = 0
-        while True:
-            line = my_file_0.readline()
-            if not line:
-                break
-            lineNum += 1
-    return lineNum
+    '''
+        Counts and returns the number of lines in a file
+    '''
+    l_count = 0
+
+    with open(filename, encoding="utf-8") as fd:
+        for line in fd:
+            l_count += 1
+    return l_count
