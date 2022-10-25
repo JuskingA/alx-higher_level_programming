c466bc8af790ca42576e4f1661e6037b5620540f
JuskingA <juskingadjeinyarko@gmail.com>
2022-10-25 21:18:08 +0100

    9 hours to go

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""Module for read_lines method"""
+"""Contain a function that appends to file"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """
-    Method that reads n lines of a text file (UTF8)
-    and prints it to stdout
-    """
-    with open(filename, encoding="utf-8") as my_file_0:
-        if nb_lines <= 0:
-            print(my_file_0.read(), end="")
-        else:
-            for line in my_file_0:
-                if nb_lines == 0:
-                    break
-                print(line, end="")
-                nb_lines -= 1
+def append_write(filename="", text=""):
+    """Handles content appendint to file"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
